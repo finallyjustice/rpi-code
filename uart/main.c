dc8a657e6f260e015f6b253b20e100971c54c4ba
zhang <zhang@Linux.(none)>
2014-09-04 01:19:35 -0400

    add cprintf

--- uart/main.c
+++ uart/main.c
@@ -13,6 +13,77 @@ void uprint(char *s)
 	uart_send('\n');
 }
 
+static void printint(int xx, int base, int sign)
+{
+	static char digits[] = "0123456789abcdef";
+	char buf[16];
+	int i;
+	unsigned int x;
+
+	if(sign && (sign = xx < 0))
+		x = -xx;
+	else
+		x = xx;
+
+	i = 0;
+	do{
+		buf[i++] = digits[x % base];
+	}while((x /= base) != 0);
+
+	if(sign)
+		buf[i++] = '-';
+
+	while(--i >= 0)
+		uart_send(buf[i]);
+}
+
+// Print to the console. only understands %d, %x, %p, %s.
+void cprintf(char *fmt, ...)
+{
+	int i, c;
+	unsigned int *argp;
+	char *s;
+
+	argp = (unsigned int*)(void*)(&fmt + 1);
+	for(i = 0; (c = fmt[i] & 0xff) != 0; i++)
+	{
+		if(c != '%')
+		{
+			if(c == '\n')
+				uart_send('\r');
+			uart_send(c);
+			continue;
+		}
+		c = fmt[++i] & 0xff;
+		if(c == 0)
+			break;
+		switch(c)
+		{
+		case 'd':
+			printint(*argp++, 10, 1);
+			break;
+		case 'x':
+		case 'p':
+			printint(*argp++, 16, 0);
+			break;
+		case 's':
+			if((s = (char*)*argp++) == 0)
+				s = "(null)";
+			for(; *s; s++)
+				uart_send(*s);
+			break;
+		case '%':
+			uart_send('%');
+			break;
+		default:
+			// Print unknown % sequence to draw attention.
+			uart_send('%');
+			uart_send(c);
+			break;
+		}
+	}
+}
+
 // cache the input from user via UART
 char buf[64];
 
@@ -21,6 +92,10 @@ int bootmain(void)
 {
 	// init uart to enable debugging with TTL
     uart_init();
+
+	printint(1234, 10, 1);
+
+	cprintf("\n Hello World %d %d\n", 1222, 3333);
 
 	// print string
 	uprint("This is the echo service:");
